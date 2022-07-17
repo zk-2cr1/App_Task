3fe896c5141529914d3486fb7ce776795aa9c66b
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-07-17 13:07:53 +0000

    [Finisha] Task2

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice: 'Book was successfully create.'
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -22,14 +23,25 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
- def update
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice: 'Book was successfully updated.'
+    else
+      @books=Book.all
+      render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
- end
+    book.destroy
+      redirect_to '/books',alert: 'Book was successfully destroyed.'
+  end
+
 
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'home/index'
+  root :to => 'homes#top'
+  resources:books
+
 end
+
